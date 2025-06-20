f4763a41f74a2051c2fb56fa8eee44853cd85f9f
Noa Yarin <yarin066@gmail.com>
2025-06-20 17:55:16 +0300

    Added register, getAllUsers and isEmailExists functions

--- Backend/BL/User.cs
+++ Backend/BL/User.cs
@@ -1,33 +1,44 @@
-﻿using System.Text;
+﻿using System.Security.Cryptography;
+using System.Text;
 
 namespace Horizon.BL
 {
     public class User
     {
-        public int Id { get; set; }
+        public int ?Id { get; set; }
         public string Email { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string BirthDate { get; set; }
         public string ImgUrl { get; set; }
-        public bool MobileUrl { get; set; }
         public bool IsAdmin { get; set; }
         public string HashedPassword { get; set; }
-        public List<User> BlockedUsers { get; set; }
-        public List<Tag> Tags { get; set; }
-        public List<Article> SavedArticles { get; set; }
+        public List<User> ?BlockedUsers { get; set; }
+        public List<Tag> ?Tags { get; set; }
+        public List<Article> ?SavedArticles { get; set; }
 
         public User() { }
 
-        public static int Register(User user)// change params to relevant parms
+        public List<User> Read()
         {
-            //user.HashedPassword = HashPassword(user.HashedPassword);
-            //pass it ot DB
-            return 0;
+            DBservices db = new DBservices();
+            return db.GetAllUsers();
+        }
+      
+       public bool Register()
+       {
+            DBservices db = new DBservices();
+            if (db.IsEmailExists(this.Email))
+            {
+                return false;
+            }
+            this.HashedPassword = HashPassword(this.HashedPassword);
+            db.InsertUser(this);
+            return true;
         }
 
-        /*
-        public static string HashPassword(string password)// change params to relvent parms
+
+        public static string HashPassword(string password)
         {
             using (SHA256 sha256 = SHA256.Create())
             {
@@ -39,22 +50,21 @@ namespace Horizon.BL
             }
 
         }
+        /*
+      public static int LogIn(string email, string password)// change params to relvent parms
+      {
 
-        public static int LogIn(string email, string password)// change params to relvent parms
-        {
+          //bring data from db and than check for each user if it is his email and password
 
-            //bring data from db and than check for each user if it is his email and password
+          if (email == user.Email && HashPassword(password) == user.Password)
+          {
+              //continue and return  
+              return 1;
+          }
 
-            if (email == user.Email && HashPassword(password) == user.Password)
-            {
-                //continue and return  
-                return 1;
-            }
-
-            return 0; // if user nor found
-        }
-        */
-
+          return 0; // if user nor found
+      }
+     */
         public static int LogOut(User user)// change params to relvent parms
         {
             //TODO DB
